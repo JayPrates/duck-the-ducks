bb08db280a0ada4b9ef2fd81cd47d8ed0389a6c8
Joao Prates <joaoprates.az@gmail.com>
2021-08-20 14:38:23 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const raceCanvas = document.getElementById("canvas");
+const context = raceCanvas.getContext("2d");
+
+
+const game 
+
+
+class Duck {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    spawn () {
+
+    }
+    move () {
+
+    }
+}
+
+
+const newDuck = new Duck(x, y);
+
+function createDucks (){
+    const maxY = 700;
+    let randomY = Math.floor(Math.random() * maxY);
+    const leftPos = 0;
+    const rightPos = 500;
+    const minSpawnTime = 10;
+    const maxSpawnTime = 30;
+
+    let leftPos = Math.floor(Math.random())
+
+}
