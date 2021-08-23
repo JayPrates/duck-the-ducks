c7a7bd2fadd448329297751763608ab3b28a7402
Joao Prates <joaoprates.az@gmail.com>
2021-08-23 10:09:58 +0100

    fixes

--- js/ducks.js
+++ js/ducks.js
@@ -1,11 +1,14 @@
 class Duck {
-    constructor(x, y){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
         this.width = 50;
         this.height = 50;
     }
-    draw(){
-        context.drawImage(this.Image, this.x, this.y, 50, 70)
+    draw() {
+        const img = new Image();
+        img.src = "./images/test_duck.png";
+        this.image = img;
+        context.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
 }
