cfc5085e54e3e4ecd8cf0d2653aa0a71d4d4ed7f
Joao Prates <joaoprates.az@gmail.com>
2021-08-26 12:08:24 +0100

    Added sounds & Game Over Alert

--- js/index.js
+++ js/index.js
@@ -5,6 +5,12 @@ document.getElementById("start-button").onclick = () => {
     startGame();
 };
 
+//Music
+const music = new Audio("./sounds/Estranged Callipoe Waltz.mp3")
+//Sounds
+const quackSound = new Audio("./sounds/Duck Quack.mp3");
+
+
 //LVL1
 let currentGame;
 let lives = 50;
@@ -14,6 +20,7 @@ function startGame() {
     cancelAnimationFrame(currentGame.animationId);
     updateCanvas();
     timeoutCheckLv2();
+    music.play();
 }
 
 function timeoutCheckLv2() {
@@ -72,7 +79,10 @@ function updateCanvas() {
     }
     if (lives === 0) {
         lives = 5;
+        music.pause();
         startLvl2 = false;
+        alert(`         Game Over!
+        Your score was: ${currentGame.score}`);
     }
     
     if (startLvl2 === true) {
@@ -234,6 +244,7 @@ canvas.addEventListener('click', (e) => {
     for(let i = 0; i<currentGame.obstacles2.length; i++) {
         console.log(`logging obstacles ${currentGame.obstacles2[i].x}, ${currentGame.obstacles2[i].y}`)
         if (currentGame.obstacles2[i].x < pointerX && (currentGame.obstacles2[i].x + currentGame.obstacles2[i].width) > pointerX && (currentGame.obstacles2[i].y - 5) < pointerY && (currentGame.obstacles2[i].y + currentGame.obstacles2[i].width) > pointerY) {
+            quackSound.play();
             console.log("This is working Right Ducks")
             console.log(currentGame.obstacles2[i]);
             if (currentGame.obstacles2[i].isDead){
@@ -247,6 +258,7 @@ canvas.addEventListener('click', (e) => {
 
     for(let i = 0; i<currentGame.obstacles.length; i++) {
         if (currentGame.obstacles[i].x < pointerX && (currentGame.obstacles[i].x + currentGame.obstacles[i].width) > pointerX && (currentGame.obstacles[i].y - 5) < pointerY && (currentGame.obstacles[i].y + currentGame.obstacles[i].width + 5) > pointerY) {
+            quackSound.play();
             console.log("This is working Left Ducks")
             console.log(currentGame.obstacles[i].x, currentGame.obstacles[i].y);
             if (currentGame.obstacles[i].isDead){
